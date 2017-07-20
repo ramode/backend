0186fd165d6368bdb6c7407a1a661a1e10c6aaea
morfair <morfair@gmail.com>
2017-07-20 17:47:53 +0300

    Add utils: return_as_json with datetime fix

---
+++ utils.py
@@ -0,0 +1,35 @@
+
+def return_as_json(handler):
+	"""
+	Это декоратор, который приводит вывод функции в HTTP JSON ответ.
+	"""
+	from aiohttp import web
+	import datetime
+
+	async def middleware_handler(request):
+		
+		res = await handler(request)
+
+		if type(res) == web.Response:
+			return res
+
+		def date_to_isoformat(item):
+			print(item)
+			for key in item.keys():
+				# val = getattr(item, key)
+				val = item[key]
+				if isinstance(val, datetime.date):
+					item[key] = val.isoformat()
+
+		# проверяем, не массив ли это:
+		if isinstance(res, list):
+
+			for item in res:
+				date_to_isoformat(item)
+
+		else:
+			date_to_isoformat(item)
+
+		return web.json_response(res)
+
+	return middleware_handler
